8941045b61350c0273dc19ab246532e0488ce40c
palimi742 <christianpalomo65@gmail.com>
2025-08-21 17:11:22 -0500

    commit initial files

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let leftBoxEl = document.getElementById("left-btn")
+let rightBoxEl = document.getElementById("right-btn")
+
+function getRandomChar(){
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    return characters[randomIndex]
+}
+
+let passwordLength = 12;
+
+function generatePassword(){
+    let randomPassword = "";
+    for(let i = 0; i < passwordLength; i++){
+        randomPassword += getRandomChar();
+    }
+    return randomPassword;
+}
+
+let randomPasswordLeftBox = generatePassword();
+let randomPasswordRightBox = generatePassword();
+
+leftBoxEl.textContent = randomPasswordLeftBox;
+rightBoxEl.textContent = randomPasswordRightBox;
+
